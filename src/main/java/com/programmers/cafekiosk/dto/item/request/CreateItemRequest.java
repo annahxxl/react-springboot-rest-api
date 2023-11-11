bdc122b389a684c300fdc6697ae0a290a3f2d90b
Hanna <hanna@trdst.com>
2023-11-12 00:26:35 +0900

    fix: validator 변경

--- src/main/java/com/programmers/cafekiosk/dto/item/request/CreateItemRequest.java
+++ src/main/java/com/programmers/cafekiosk/dto/item/request/CreateItemRequest.java
@@ -2,21 +2,21 @@ package com.programmers.cafekiosk.dto.item.request;
 
 import com.programmers.cafekiosk.dto.item.validator.ValidItemType;
 import com.programmers.cafekiosk.entity.item.ItemType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateItemRequest(
 
-        @NotNull
+        @NotBlank
         @ValidItemType
         ItemType type,
 
-        @NotNull
+        @NotBlank
         String name,
 
         @NotNull
         Long price,
 
-        @NotNull
         String description
 ) {
 }
--- src/main/java/com/programmers/cafekiosk/dto/item/request/UpdateItemRequest.java
+++ src/main/java/com/programmers/cafekiosk/dto/item/request/UpdateItemRequest.java
@@ -4,25 +4,25 @@ import com.programmers.cafekiosk.dto.item.validator.ValidItemStatus;
 import com.programmers.cafekiosk.dto.item.validator.ValidItemType;
 import com.programmers.cafekiosk.entity.item.ItemStatus;
 import com.programmers.cafekiosk.entity.item.ItemType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateItemRequest(
 
-        @NotNull
+        @NotBlank
         @ValidItemType
         ItemType type,
 
-        @NotNull
+        @NotBlank
         @ValidItemStatus
         ItemStatus status,
 
-        @NotNull
+        @NotBlank
         String name,
 
         @NotNull
         Long price,
 
-        @NotNull
         String description
 ) {
 }
