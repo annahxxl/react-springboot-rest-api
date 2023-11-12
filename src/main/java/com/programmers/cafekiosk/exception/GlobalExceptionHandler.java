7306752d8302bd9be126c7035229db79749e81ec
Hanna <hanna@trdst.com>
2023-11-12 15:28:02 +0900

    fix: handleMethodArgumentNotValid 오버라이드

--- src/main/java/com/programmers/cafekiosk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/cafekiosk/exception/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.programmers.cafekiosk.exception;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -22,15 +20,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Throwable cause = ex.getCause();
-
-        if (cause instanceof InvalidFormatException) {
-            InvalidFormatException invalidFormatException = (InvalidFormatException) cause;
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid value for Enum: " + invalidFormatException.getValue()));
-        }
-
-        return super.handleHttpMessageNotReadable(ex, headers, status, request);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity.status(status).body(ErrorResponse.of(status.value(), ex.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
     }
 
     @Override
